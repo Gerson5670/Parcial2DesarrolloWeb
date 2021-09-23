a4f82f497a15f5d0adfe1106e70bc6af804834fe
Gerson5670 <74474687+Gerson5670@users.noreply.github.com>
2021-09-23 09:21:44 -0600

    Update crud_producto.php

--- Parcial2DesarrolloWeb/crud_producto.php
+++ Parcial2DesarrolloWeb/crud_producto.php
@@ -1,8 +1,6 @@
 <?php
 
-
 include('../app/config/config.php');
-
 session_start();
 
 
@@ -19,8 +17,29 @@ if (isset($_SESSION['u_usuario'])){
 
 }
 
-$productos=$_POST['productos'];
-$idMarca = $_POST ['idMarca'];
-$Descripcion =$_POST ['Descripcion'];
-$precio_costo =$_POST ['precio_costo'];
+
+$producto=$_POST ['producto'];
+$idMarca=$_POST['idMarca'];
+$descripcion = $_POST ['Descripciom'];
+$precio_costo=$_POST ['precio_costo'];
 $precio_venta=$_POST ['precio_venta'];
+$existencia=$_POST ['existencia'];
+
+$sentencia1=$pdo->prepare ("INSERT INTO producto (producto, idMarca, Descripcion, precio_costo, precio_venta, existencia)
+VALUES(:producto, :idMarca, :Descripcion, :precio_costo, :precio_venta, :existencia)");
+
+$sentencia1->bindParam(':producto',$producto);
+$sentencia1->bindParam(':idMarca',$idMarca);
+$sentencia1->bindParam(':Descripcion',$descripcion);
+$sentencia1->bindParam(':precio_costo',$precio_costo);
+$sentencia1->bindParam(':precio_venta',$precio_venta);
+$sentencia1->bindParam(':existencia',$existencia);
+
+
+if ($sentencia1->execute()){
+   // echo "usuario insertado a la base de datos";
+ header("location: index.php");
+}else{
+ echo "No se ha insertado ningun dato";
+
+}
