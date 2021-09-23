adfba2b957d0f05b88cf87f552807899e2e9beb7
Gerson5670 <74474687+Gerson5670@users.noreply.github.com>
2021-09-23 09:22:32 -0600

    Update crud_producto.php

--- Parcial2DesarrolloWeb/crud_producto.php
+++ Parcial2DesarrolloWeb/crud_producto.php
@@ -1,6 +1,6 @@
 <?php
+include '../app/config/config.php';
 
-include('../app/config/config.php');
 session_start();
 
 
@@ -16,30 +16,109 @@ if (isset($_SESSION['u_usuario'])){
         </script>";
 
 }
+ 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Creacion de productos  </title>
+<?php include ('../layout/head.php'); ?>
+</head>
+<body class="hold-transition skin-green sidebar-mini">
+<div class="wrapper">
+
+<?php include ('../layout/menu.php'); ?>
+
+  <div class="content-wrapper">  
+    <section class="content-header">
+    
+     </section>
+<!--      ------------------------------------------------------------------------             -->
+ 
+    <section class="content">
+  
+    <div class="row">
+                    <div class="col-md-12">
+                        <div class="panel panel-success class">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">
+                                    Ingresos de productos</h3>
+                            </div>
+                            <div class="panel-body">
+                                <form action="create_ProductoController.php" method="post" enctype="multipart/form-data">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for=""><i class="glyphicon glyphicon-list-alt"></i> Producto </label>
+                                                <input type="text" class="form-control" name="NitClie" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for=""> <i class="glyphicon glyphicon-user"></i> Id Marca</label>
+                                                <input type="text" class="form-control" name="NombresClie" required>
+                                            </div>
+                                          
+                                            <div class="form-group">
+                                                <label for=""> <i class="glyphicon glyphicon-user"></i> Descripcion </label>
+                                                <input type="text" class="form-control" name="ApellidosClie" >
+                                            </div>
+                                                     
 
 
-$producto=$_POST ['producto'];
-$idMarca=$_POST['idMarca'];
-$descripcion = $_POST ['Descripciom'];
-$precio_costo=$_POST ['precio_costo'];
-$precio_venta=$_POST ['precio_venta'];
-$existencia=$_POST ['existencia'];
 
-$sentencia1=$pdo->prepare ("INSERT INTO producto (producto, idMarca, Descripcion, precio_costo, precio_venta, existencia)
-VALUES(:producto, :idMarca, :Descripcion, :precio_costo, :precio_venta, :existencia)");
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label for=""><i class="glyphicon glyphicon-phone"></i> Precio Costo</label>
+                                                <input type="text" class="form-control" name="CelularClie" >
+                                            </div>
+                                         
+                                
+                                            <div class="form-group">
+                                                <label for=""><i class="glyphicon glyphicon-list-alt"></i> Precio venta </label>
+                                                <input type="text" class="form-control" name="DireccionClie" >
+                                            </div>
 
-$sentencia1->bindParam(':producto',$producto);
-$sentencia1->bindParam(':idMarca',$idMarca);
-$sentencia1->bindParam(':Descripcion',$descripcion);
-$sentencia1->bindParam(':precio_costo',$precio_costo);
-$sentencia1->bindParam(':precio_venta',$precio_venta);
-$sentencia1->bindParam(':existencia',$existencia);
+                                            <div class="form-group">
+                                                <label for=""><i class="glyphicon glyphicon-list-alt"></i> Existencia </label>
+                                                <input type="text" class="form-control" name="DireccionClie" >
+                                            </div>
+                                           
+
+                                          
+                                           
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <br>
+                                        <div class="ol-md-12">
+                                            <center>
+                                                <a href="" class="btn btn-default btn-lg">Cancelar</a>
+                                                <input type="submit" class="btn btn-success btn-lg" value="Registrar">
+                                            </center>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- /.content -->
+        </div>
+    
+</div>
+
+       </section>  
+  </div>
+ 
+<?php include ('../layout/footer.php'); ?>  
 
 
-if ($sentencia1->execute()){
-   // echo "usuario insertado a la base de datos";
- header("location: index.php");
-}else{
- echo "No se ha insertado ningun dato";
+<?php include ('../layout/aside.php'); ?>
+  
+<?php include ('../layout/footer_link.php'); ?>
 
-}
+</div>
+</body>
+</html>
